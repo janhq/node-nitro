eb6bac89d8064cf07d44d8ef70f7c94005b59176
hiento09 <136591877+hiento09@users.noreply.github.com>
2023-11-29 08:47:03 +0700

    Initiate node-nitro as npm package (#1)
    * Initiate
    
    * Change windows to bat script
    
    * Add bin symblink
    
    * Add bin symblink
    
    * Add bin symblink for windows
    
    * Add nitro.js script for symblink
    
    * Update readme for node-nitro
    
    * Add CICD
    
    ---------
    
    Co-authored-by: Hien To <hien@jan.ai>

---
+++ download.bat
@@ -0,0 +1,10 @@
+@echo off
+if "%NITRO_VERSION%"=="" (
+    for /f "delims=" %%i in ('node -p "require('./package.json').nitroVersion"') do set NITRO_VERSION=%%i
+)
+
+if "%GPU%" == "true" (
+    .\node_modules\.bin\download https://github.com/janhq/nitro/releases/download/v%NITRO_VERSION%/nitro-%NITRO_VERSION%-win-amd64-cuda.zip -e --strip 1 -o .
+) else (
+    .\node_modules\.bin\download https://github.com/janhq/nitro/releases/download/v%NITRO_VERSION%/nitro-%NITRO_VERSION%-win-amd64.zip -e --strip 1 -o .
+)
